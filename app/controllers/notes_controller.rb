090ab9a2befe8f1bf3b3e8b8af88578a869fb035
Sydney Orr <sydneywhitters@gmail.com>
2021-08-23 15:47:35 -0600

    initial commit

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,14 @@
+class NotesController < ApplicationController
+#  def index
+#  end
+end
+
+
+
+# def index
+#   @notes = Note.all
+#   render component: "Notes", props:{notes: @notes}
+# end
+
+# def show
+# end
